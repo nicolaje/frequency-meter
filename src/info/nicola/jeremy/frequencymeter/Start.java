e70f97cbbb248567dc3b0cffe0b6d626dd3af20e
Jeremy Nicola <jeremy.nicola@gmail.com>
2012-11-15 18:01:13 +0100

    Added possiblity to set FSample & Buffer_Size via command parameters

--- src/info/nicola/jeremy/frequencymeter/Start.java
+++ src/info/nicola/jeremy/frequencymeter/Start.java
@@ -13,7 +13,18 @@ public class Start {
 		view.pack();
 		view.setVisible(true);
 		if(args.length==2){
-			
+			try{
+				int bufferLength=Integer.parseInt(args[0]);
+				int fSample=Integer.parseInt(args[1]);
+				boolean fSampleOK=fSample==44100||fSample==22050||fSample==11025||fSample==8000||fSample==48000;
+				if(fSampleOK&&bufferLength>=2){
+					new Analyzer(view, bufferLength, fSample).start();
+				}else{
+					new Analyzer(view, BUFFER_LENGTH, Fs).start();
+				}
+			}catch(NumberFormatException e){
+				new Analyzer(view, BUFFER_LENGTH, Fs).start();
+			}
 		}else{
 			new Analyzer(view, BUFFER_LENGTH, Fs).start();
 		}
